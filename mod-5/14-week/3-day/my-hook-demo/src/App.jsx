83a7a65e65d42b5fbf0b731d5545e4ae596fd250
w-duffy <w.duffy@outlook.com>
2024-10-02 11:13:45 -0500

    added w14d3 notes

---
+++ mod-5/14-week/3-day/my-hook-demo/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  // let spots = { id: 1 };
+  // console.log("did I render");
+  useEffect(() => {
+    console.log("did I render");
+
+    // spots.allSpots = [1, 2, 3]; // think of this as fetching your spots
+    
+
+  }, [count]);
+
+  // if (spots.nums === undefined) return null;
+  // console.log(spots.allSpots.map);
+
+  return (
+    <>
+      <h1>Hello Vite + React!</h1>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is: {count}
+      </button>
+    </>
+  );
+}
+
+export default App;
