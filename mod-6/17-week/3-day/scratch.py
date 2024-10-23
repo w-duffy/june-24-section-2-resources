52f7210351bd7cf564656aff9fea1fd87f874d7e
w-duffy <w.duffy@outlook.com>
2024-10-23 11:01:19 -0500

    adds w17d3 intro demo

---
+++ mod-6/17-week/3-day/scratch.py
@@ -0,0 +1,34 @@
+spot = {
+    "id": 1,
+    (12135, "cool"): "123 Disney ln",
+    "price": 350,
+    "reviews": [
+        {"user": "demo", "text": "Great job!"},
+        {"user": "will", "text": "Python is awesome"},
+        {"user": "zav", "text": "What framework did you use?"},
+    ],
+}
+
+
+# filtered_reviews = []
+# for review in spot['reviews']:
+#     if review['user'] != 'will' and review['user'] != 'zav':
+#         print("user names !", review['user'])
+        # filtered_reviews.append(review['user'])
+
+
+filtered_reviews = [review
+                    for review in spot["reviews"]
+                    if review['user'] != 'will' and review['user'] != 'zav']
+
+
+print("Filtered rev", filtered_reviews)
+
+
+# # new_num_list = [num + 1
+#                for num in num_list]
+
+# # for num in num_list:
+#     # new_num_list.append(num + 1)
+
+# print("\nNEW LIST\n ", new_num_list)
